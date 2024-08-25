9d4e6461fdb62a73ea9b1ab17aaae22412173d84
Rushs321 <fyggh566@tutamail.com>
2024-08-25 18:52:48 +0530

    Update bypass.js

--- src/bypass.js
+++ src/bypass.js
@@ -1,5 +1,19 @@
 export function bypass(request, reply, stream) {
+    // Set initial headers
     reply.header('x-proxy-bypass', 1);
+
+    // If the content length is known, we set it; otherwise, we stream without it
+    const contentLength = request.params.originSize;
+
+    if (contentLength) {
+        reply.header('content-length', contentLength);
+    }
+
+    // Set content-type header if it's available
+    const contentType = request.params.originType;
+    if (contentType) {
+        reply.header('content-type', contentType);
+    }
 
     // Pipe the input stream directly to the reply (response) stream
     reply.code(200);
